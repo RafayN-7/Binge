bc709fa8605f5a33a9c126f528617bc847a0153c
RafayN-7 <naumanrafay7@gmail.com>
2025-07-26 19:46:49 +0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,20 @@
 import { Route, Routes } from "react-router";
 import Navbar from "./components/Navbar";
 import Homepage from "./Pages/Homepage";
+import Videoplayer4 from "./Pages/Videoplayer4";
 import Moviepage from "./Pages/Moviepage";
+import Videoplayer3 from "./Pages/Videoplayer3";
 import "video.js/dist/video-js.css"; 
 import videojs from "video.js";
 import { useState, useEffect, useRef } from "react";
 import Movies from "./Pages/Movies";
+import Videoplayer2 from "./Pages/Videoplayer2";
 import TvSeries from "./Pages/TvSeries";
 import TvShowPage from "./Pages/TvShowPage";
 import SearchPage from "./Pages/SearchPage";
-import Videoplayer from "./Pages/Videoplayer"; // Import the new VideoPlayer component
-import Videoplayer2 from "./Pages/Videoplayer2"; // Import the new VideoPlayer component
-import Videoplayer3 from "./Pages/Videoplayer3"; // Import the new VideoPlayer component
-import Videoplayer4 from "./Pages/Videoplayer4"; // Import the new VideoPlayer component
-
-
-
-
+import Videoplayer from "./Pages/Videoplayer";
 
 export const App = () => {
-
-  
 
   return (
     <div>
@@ -34,9 +28,9 @@ export const App = () => {
         <Route path={"/tv/:id"} element= {<TvShowPage/>} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/Videoplayer/:id" element={<Videoplayer/>} />
-        <Route path="/Videoplayer2/:id" element={<Videoplayer2/>} />
-        <Route path="/Videoplayer3/:id" element={<Videoplayer3/>} />
-        <Route path="/Videoplayer4/:id" element={<Videoplayer4/>} />
+        <Route path="/Videoplayer/:id" element={<Videoplayer2/>} />
+        <Route path="/Videoplayer/:id" element={<Videoplayer3/>} />
+        <Route path="/Videoplayer/:id" element={<Videoplayer4/>} />
 
 
        </Routes>  
